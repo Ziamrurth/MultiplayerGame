7f20ed75bb57eec3a4a6aba97aaaadaa1cc76d94
ArkashaPygachev <93211239@mail.ru>
2020-12-31 10:23:25 +0300

    initial commit

---
+++ MultiplayerGame/Assets/Scripts/Guns.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guns : NetworkBehaviour {
+
+    float rotationSpeed = 20f;
+
+    public void RotateOverTime()
+    {
+        transform.Rotate(Vector3.back * (rotationSpeed * Time.deltaTime));
+    }
+
+    void Update()
+    {
+        RotateOverTime();
+    }
+}
---
+++ MultiplayerGame/Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using Cinemachine;
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : NetworkBehaviour {
+
+    [SerializeField]
+    float speedScale = 1f;
+
+    float moveHorizontal = 0;
+    float moveVertical = 0;
+
+    [SerializeField]
+    CinemachineVirtualCamera mainCamera;
+
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            Vector3 movement = new Vector3(moveHorizontal * speedScale * Time.deltaTime, moveVertical * speedScale * Time.deltaTime, 0);
+            transform.position += movement;
+        }
+    }
+
+    void RandomiseMovement()
+    {
+        float angle = Random.Range(0f, 360f);
+        moveVertical = Mathf.Sin(angle);
+        moveHorizontal = Mathf.Cos(angle);
+    }
+
+    void MountCamera()
+    {
+        if (isLocalPlayer)
+        {
+            mainCamera = GameObject.Find("CameraController").GetComponent<CameraController>().mainVirtualCamera;
+
+            mainCamera.LookAt = transform;
+            mainCamera.Follow = transform;
+        }
+    }
+
+    void Start()
+    {
+        MountCamera();
+        RandomiseMovement();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    private void OnMouseDown()
+    {
+        if (isLocalPlayer)
+        {
+            RandomiseMovement();
+        }
+    }
+}
